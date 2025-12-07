354b92a644701eac65d8aeebc979096520d492e4
all4null <jjm2008@daum.net>
2025-12-08 08:02:40 +0900

    fix Cors2

--- backend/src/main/java/com/hellofood/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/hellofood/backend/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +43,7 @@ public class SecurityConfig {
                     chain.doFilter(request, response);
                 }, ChannelProcessingFilter.class)
 
-                
+
                 // CORS 활성화 및 설정 등록
                 .cors(Customizer.withDefaults())
 
